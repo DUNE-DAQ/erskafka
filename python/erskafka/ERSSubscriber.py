ac8c9116a84fc3d4d6e619d3b561b154738c1832
Marco Roda <mroda@liverpool.ac.uk>
2023-06-09 12:33:23 +0200

    More logic implemented

--- python/erskafka/ERSSubscriber.py
+++ python/erskafka/ERSSubscriber.py
@@ -4,25 +4,31 @@ import issue_pb2
 from kafka import KafkaConsumer
 import json
 from threading import Thread
+import socket
+import os
 
 class  ERSSubscriber:
     def __init__(self, config) :
-        bootstrap = config["bootstrap"]
+        self.bootstrap = config["bootstrap"]
+        if ( 'group_id' in config.keys() ) :
+            self.group = config["group_id"]
+        else:
+            self.group = ""
+        self.timeout = config["timeout"]
+        self.running = False
         self.functions = dict()
         self.thread = Thread(target=message_loop, args=self)
-        self.running = False
+
+                        
     #    print("From Kafka server:",bootstrap)
 
-    
-    #    consumer = KafkaConsumer(bootstrap_servers=bootstrap,
-     #                            group_id=kafka_consumer_group, 
-      #                           client_id=kafka_consumer_id,
-       #                          consumer_timeout_ms=kafka_timeout)
-
-       
-
-    def default_id(self):
-        
+    def default_id(self):  
+        node = socket.gethostname()
+        process = os.getpid()
+        thread = threading.get_ident()
+        id = "{}-{}-{}".format(node, process, thread)
+        return id
+           
     def add_callback(self, function, name, selection):
 
     def clear_callbacks(self):
@@ -41,8 +47,23 @@ class  ERSSubscriber:
 
 
     def message_loop(self) :
+        if (self.group == ""): group_id = self.default_id()
+        else: group_id = self.group
+
+        consumer = KafkaConsumer(bootstrap_servers=self.bootstrap,
+                                 group_id=group_id, 
+                                 client_id=self.default_id(),
+                                 consumer_timeout_ms=self.timeout)
+        
         while ( self.running ) :
-
-
+            
+            try:
+                message_it = iter(consumer)
+                message = next(message_it)
+                
+            except :
+                pass
+            
+            
 
         
