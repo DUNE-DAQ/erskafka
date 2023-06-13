4134b285d1148ddeab25994f01361401fed6eda0
Marco Roda <mroda@liverpool.ac.uk>
2023-06-13 18:42:18 +0200

    Add scheleton for test application

---
+++ test/apps/ers_publisher_test.cxx
@@ -0,0 +1,17 @@
+
+/* This application creats a ERSProducer and dumps regular messages via the system
+   
+ */
+
+#include <erskafka/ERSPublisher.hpp>
+
+using namespace std;
+using namespace dunedaq::erskafka;
+
+int main( int argc, char * argv[] ) {
+
+
+
+  
+  return 0 ;
+}
