7c08ae9948a35c13eac8247dd230c9a45f02da15
Stefano Vergani <svergani@np04-srv-011.cern.ch>
2023-10-17 17:39:43 +0200

    new way of automatically creating errors in ERSPublisher

--- python/erskafka/ERSPublisher.py
+++ python/erskafka/ERSPublisher.py
@@ -1,15 +1,14 @@
-from kafka import KafkaProducer
+import os
+import socket
+import inspect
 import ers.issue_pb2 as ersissue
+from datetime import datetime
+from kafka import KafkaProducer
 
 class ERSPublisher:
+
     def __init__(self, config):
-        """Initialize the ERSPublisher with given Kafka configurations.
-        
-        Args:
-            config (dict): Configuration for Kafka producer. 
-                           Should contain 'bootstrap' for the Kafka bootstrap server.
-                           Optionally contains 'topic' for the default topic to publish to.
-        """
+        """Initialize the ERSPublisher with given Kafka configurations."""
         self.bootstrap = config['bootstrap']
         self.topic = config.get('topic', 'ers_stream')
         self.producer = KafkaProducer(
@@ -17,31 +16,64 @@ class ERSPublisher:
             value_serializer=lambda v: v.SerializeToString(),
             key_serializer=lambda k: str(k).encode('utf-8')
         )
-#del method is not determinist and not best practise, but it is the best option to clean the memory 
-#without having the user to call the close method
-    def __del__(self):
-        """Destructor-like method to clean up resources."""
-        self.close()
-#the close method can be called by the user if they want to, but it will be dealt by del anyway
+
+    def _generate_context(self):
+        """Private method to generate the context for an issue."""
+        return ersissue.Context(
+            cwd=os.getcwd(),
+            file_name=__file__,
+            function_name=inspect.currentframe().f_back.f_code.co_name,  # getting the caller function name
+            host_name=socket.gethostname(),
+            line_number=inspect.currentframe().f_back.f_lineno,  # getting the caller's line number
+            package_name="erskafka"
+        )
+
+    def create_issue(self, message, name="DefaultIssueName", severity=None):
+        """Create an ERS issue with minimal user input."""
+        current_time = datetime.now().timestamp()
+        context = self._generate_context()
+        issue = ersissue.SimpleIssue(
+            context=context,
+            name=name,
+            message=message,
+            time=current_time
+        )
+        if severity:
+            issue.severity = severity
+        return issue
+
+    def publish_simple_message(self, message):
+        issue = self.create_issue(message)
+        issue_chain = ersissue.IssueChain(
+            final=issue,
+            session=str(issue.time)  # using time as a session identifier
+        )
+        return self.publish(issue_chain)
+
+    def publish(self, issue):
+        """Publish an ERS issue to the Kafka topic."""
+        return self.producer.send(self.topic, key=issue.session, value=issue)
+#this can be called by the user (it is best practise), but it is dealt by del if not
     def close(self):
         """Explicit method to close the Kafka producer."""
         if self.producer:
             self.producer.close()
+#del is not determinist and not best practise, but if the user does not call close, it will close and clean up resources
+    def __del__(self):
+        """Destructor-like method to clean up resources."""
+        self.close()
 
-    def publish(self, issue):
-        """Publish an ERS issue to the Kafka topic.
-        
-        Args:
-            issue (ers.issue_pb2.IssueChain): The issue to be published.
-        
-        Returns:
-            FutureRecordMetadata: Result from Kafka producer send method.
-        """
-        return self.producer.send(self.topic, key=issue.session, value=issue)
+class ERSException(Exception):
+    """Custom exception which can also be treated as an ERS issue."""
+    
+    def __init__(self, message):
+        super().__init__(message)
+        self.message = message
 
 # Usage example:
 # publisher = ERSPublisher(config)
-# publisher.publish(issue)
+# publisher.publish_simple_message("This is a simple message.")
 # No need to manually close, but you can if desired:
 # publisher.close()
+
 
